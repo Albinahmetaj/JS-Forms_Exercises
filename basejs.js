b068474143a758e18d803fe93366496165ec3367
Albinahmetaj <albingithub@gmail.com>
2021-04-29 10:42:55 +0200

    Update basejs.js

--- basejs.js
+++ basejs.js
@@ -1,10 +1,10 @@
-
 let currentAddress = window.location.href;
 
 let currentURL = new URL(window.location.href);
 
 
 let searchParams = new URLSearchParams(currentURL.search);
+
 
 
 let fname = searchParams.get("fname");
@@ -14,12 +14,22 @@ let Mr = searchParams.get("Mr");
 let Ms = searchParams.get("Ms");
 let birthday = searchParams.get("birthday");
 let age = searchParams.get("resultBday");
+let button = searchParams.get("btn");
 
+
+if(!fname){
+    button.addEventListener("click",function(){
+        localStorage.name = fname.value;
+    });
+}
+document.write(localStorage.name);
+
+localStorage.getItem('fname');
+document.write(localStorage.fname);
 
 console.log("Firstname: " + fname);
 console.log("Lastname: " + lname);
 console.log("E-mail: " + email);
-console.log("age: " + age);
 //console.log(Mr);
 //console.log(Ms);
 console.log("Birthdate: " + birthday);
